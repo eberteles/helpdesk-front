c38b75e19b9b1c3492441263ca4af3c5661cba81
eberteles <eberteles@hotmail.com>
2022-02-01 15:53:57 -0300

    listar chamados

---
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Chamado } from 'src/app/models/chamado';
+import { ChamadoService } from 'src/app/services/chamado.service';
+
+@Component({
+  selector: 'app-chamado-list',
+  templateUrl: './chamado-list.component.html',
+  styleUrls: ['./chamado-list.component.css']
+})
+export class ChamadoListComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'titulo', 'dataAbertura', 'cliente', 'tecnico', 'prioridade', 'status', 'acoes'];
+  dataSource = new MatTableDataSource<Chamado>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private service: ChamadoService
+  ) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    this.service.findAll().subscribe( resposta => {
+      this.dataSource = new MatTableDataSource<Chamado>(resposta);
+      this.dataSource.paginator = this.paginator;
+    } )
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+}
